49bc4bf72634d17b308a3535a521335fe8afcb3b
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2025-04-19 00:18:41 +0530

    some changes

--- src/components/modules/auth/register/RegisterForm.tsx
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 import {
   Form,
   FormControl,
